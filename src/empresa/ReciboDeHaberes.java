70ae67a89209cae432ce8385bb59c9aa09aa0faa
Pablo Orono <oronopablo@hotmail.com.ar>
2020-04-15 17:39:01 -0300

    primera version ReciboDeHaberes

---
+++ src/empresa/ReciboDeHaberes.java
@@ -0,0 +1,39 @@
+package empresa;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class ReciboDeHaberes {
+	
+	private String nombreEmp;
+	private String direccionEmp;
+	private LocalDate fecha;
+	private Integer sueldoBruto;
+	private Integer sueldoNeto;
+	private ArrayList<String> degloceDeConceptos;
+	
+	public ReciboDeHaberes(String nombreEmp, String direccionEmp, Integer sueldoBruto, Integer sueldoNeto, ArrayList<String> degloceDeConceptos) {
+		
+		this.nombreEmp = nombreEmp;
+		this.direccionEmp = direccionEmp;
+		this.fecha = LocalDate.now();
+		this.sueldoBruto = sueldoBruto;
+		this.sueldoNeto = sueldoNeto;
+		this.degloceDeConceptos = degloceDeConceptos;
+		
+	}
+	
+	public ArrayList<String> getDegloceDeConceptos(){
+		return this.degloceDeConceptos;
+	}
+	
+	public String getNombre() {
+		return this.nombreEmp;
+	}
+
+
+	public Integer getSueldoBruto() {
+		// TODO Auto-generated method stub
+		return this.sueldoBruto;
+	}
+}
