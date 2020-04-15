a8a72ed32700fd2cf0c82a43a6bda8a8dce321e2
Pablo Orono <oronopablo@hotmail.com.ar>
2020-04-15 17:38:51 -0300

    primera version Contratado

---
+++ src/empresa/Contratado.java
@@ -0,0 +1,48 @@
+package empresa;
+
+import java.util.ArrayList;
+
+public class Contratado extends Empleado {
+
+	private Integer nroContrato;
+	private String medioDePaog;
+	private ArrayList<String> conceptos;
+	
+	public Contratado(String nombre, String direccion, Boolean estadoCivil, String fechaNacimiento,
+			Integer sueldoBasico, Integer nroContrato, String medioDePago) {
+		// TODO Auto-generated constructor stub
+		
+		super(nombre, direccion, estadoCivil, fechaNacimiento, sueldoBasico);
+		this.nroContrato = nroContrato;
+		this.medioDePaog = medioDePago;
+		this.conceptos = new ArrayList<String>();
+	}
+	
+	public Integer retenciones() {
+		return 50;
+	}
+	
+	public Integer sueldoBruto() {
+		return this.getSueldoBasico();
+	}
+	
+	public Integer sueloNeto() {
+		return this.sueldoBruto()-this.retenciones();
+	}
+	
+	public void registrarConceptos(String concepto) {
+		this.conceptos.add(concepto);
+	}
+	
+	public void generarConceptos() {
+		
+		this.registrarConceptos("Sueldo Basico: " + this.getSueldoBasico());
+		this.registrarConceptos("Retenciones: " + this.retenciones());
+		
+	}
+	
+	public ArrayList<String> getConceptos(){
+		return this.conceptos;
+	}
+
+}
